9e9cb10a94e18ae3934bf5689359872b48ae8a1c
jmayta1984 <jmayta@pucp.edu.pe>
2025-05-07 11:24:47 -0500

    feat: initial project setup

--- InventoryCompose/app/src/main/java/pe/isil/inventorycompose/ui/theme/Theme.kt
+++ InventoryCompose/app/src/main/java/pe/isil/inventorycompose/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package pe.isil.inventorycompose.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
