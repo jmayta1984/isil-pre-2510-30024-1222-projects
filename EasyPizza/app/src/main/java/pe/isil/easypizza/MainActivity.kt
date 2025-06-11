4ec012025e62f61957ef8c7195c8f175595d8d91
jmayta1984 <jmayta@pucp.edu.pe>
2025-06-11 13:13:32 -0500

    refactor: update project structure

---
+++ EasyPizza/app/src/main/java/pe/isil/easypizza/MainActivity.kt
@@ -0,0 +1,26 @@
+package pe.isil.easypizza
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.isil.easypizza.ui.theme.EasyPizzaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EasyPizzaTheme {
+
+            }
+        }
+    }
+}
