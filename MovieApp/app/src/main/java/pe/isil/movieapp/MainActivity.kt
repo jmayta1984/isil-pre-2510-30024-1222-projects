bee2efdd4b8037e18a37e86c4acd2ea12a178d32
jmayta1984 <jmayta@pucp.edu.pe>
2025-07-09 11:42:18 -0500

    chore: update project

--- MovieApp/app/src/main/java/pe/isil/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/pe/isil/movieapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import pe.isil.movieapp.presentation.Navigation
 import pe.isil.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Navigation()
             }
         }
     }
--- MovieApp/app/src/main/java/pe/isil/movieapp/presentation/Navigation.kt
+++ MovieApp/app/src/main/java/pe/isil/movieapp/presentation/Navigation.kt
@@ -4,15 +4,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
+@Preview
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
@@ -30,10 +37,28 @@ fun Navigation() {
         )
     )
 
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
+
     Scaffold (
         bottomBar = {
             NavigationBar {
-
+                navigationItems.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = index == selectedIndex.value,
+                        icon = {
+                            Icon(item.icon, contentDescription = null)
+                        },
+                        onClick = {
+                            selectedIndex.value = index
+                            navController.navigate(navigationItems[index].route)
+                        },
+                        label = {
+                            Text(item.name)
+                        }
+                    )
+                }
             }
         }
     )
